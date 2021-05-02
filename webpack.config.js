0f7c805a8b18399c7f3760a16037828a6d1fc399
shuta13 <hollystarsun@gmail.com>
2021-05-02 21:53:32 +0900

    chore: init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (_, argv) => ({
+  mode: argv.mode,
+  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  entry: {
+    ui: './src/ui.tsx',
+    code: './src/code.ts',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|webp|svg|zip)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+      chunks: ['ui'],
+      inlineSource: '.(js)$',
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+  ],
+});
