6be7811333ba2f571710eeb19a1ef88740af9d7b
shuta13 <hollystarsun@gmail.com>
2021-05-04 03:34:53 +0900

    chore: lint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  globals: {
+    figma: 'readonly',
+    __html__: 'readonly',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
