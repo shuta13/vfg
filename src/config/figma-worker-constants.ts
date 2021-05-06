bf25aaf12318df5690eb2abb3cf1ab812d063d09
shuta13 <hollystarsun@gmail.com>
2021-05-07 03:09:56 +0900

    wip

--- src/config/figma-worker-constants.ts
+++ src/config/figma-worker-constants.ts
@@ -1,7 +1,12 @@
 export const WorkspaceConstants = {
   namePrefix: '[Workspace] ',
   width: 240,
-  height: 240,
+  height: 640,
   margin: 16,
-  opacity: 0,
 } as const;
+
+export const PreviewConstants = {
+  name: 'Preview',
+  width: WorkspaceConstants.width,
+  height: WorkspaceConstants.height / 4,
+};
--- src/figma-worker/workspace.ts
+++ src/figma-worker/workspace.ts
@@ -1,5 +1,5 @@
 import { Msg } from '../types';
-import { WorkspaceConstants } from '../config';
+import { PreviewConstants, WorkspaceConstants } from '../config';
 
 export const createWorkspace = (msg: Msg) => {
   const nodesPositionX = figma.currentPage.children.map(
@@ -12,10 +12,17 @@ export const createWorkspace = (msg: Msg) => {
 
   const newNodes = [];
   const workspace = figma.createFrame();
+  // workspace frame
   workspace.name = WorkspaceConstants.namePrefix + msg.workspaceName;
   workspace.resize(WorkspaceConstants.width, WorkspaceConstants.height);
   workspace.x = lastNodePosition + WorkspaceConstants.margin;
-  workspace.opacity = WorkspaceConstants.opacity;
+
+  // preview
+  const preview = figma.createRectangle();
+  preview.name = PreviewConstants.name;
+  preview.resize(PreviewConstants.width, PreviewConstants.height);
+  workspace.appendChild(preview);
+
   figma.currentPage.appendChild(workspace);
 
   newNodes.push(workspace);
@@ -33,4 +40,12 @@ export const removeWorkspace = (msg: Msg) => {
     .forEach((node) => {
       node.remove();
     });
+};
+
+export const createPreview = (msg: Msg) => {
+  figma.currentPage.findAll(
+    (node) =>
+      node.type === 'FRAME' &&
+      node.name === WorkspaceConstants.namePrefix + msg.workspaceName
+  );
 };
