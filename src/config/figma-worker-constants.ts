0e99a1bbbe293ac81322cd597abae2e9bbf3112a
shuta13 <hollystarsun@gmail.com>
2021-05-07 22:55:29 +0900

    update: preview

--- src/config/figma-worker-constants.ts
+++ src/config/figma-worker-constants.ts
@@ -1,12 +1,12 @@
 export const WorkspaceConstants = {
-  namePrefix: '[Workspace] ',
-  width: 240,
-  height: 640,
+  suffix: 'Workspace',
+  width: 320,
+  height: 720,
   margin: 16,
 } as const;
 
 export const PreviewConstants = {
-  name: 'Preview',
+  suffix: 'Preview',
   width: WorkspaceConstants.width,
   height: WorkspaceConstants.height / 4,
 };
--- src/figma-worker/workspace.ts
+++ src/figma-worker/workspace.ts
@@ -11,31 +11,42 @@ export const createWorkspace = (msg: Msg) => {
       : 0;
 
   const newNodes = [];
+
+  // workspace
   const workspace = figma.createFrame();
-  // workspace frame
-  workspace.name = WorkspaceConstants.namePrefix + msg.workspaceName;
+  workspace.name = `[${msg.workspaceName}] ${WorkspaceConstants.suffix}`;
   workspace.resize(WorkspaceConstants.width, WorkspaceConstants.height);
   workspace.x = lastNodePosition + WorkspaceConstants.margin;
 
   // preview
-  const preview = figma.createRectangle();
-  preview.name = PreviewConstants.name;
+  const preview = figma.createFrame();
+  preview.name = `[${msg.workspaceName}] ${PreviewConstants.suffix}`;
   preview.resize(PreviewConstants.width, PreviewConstants.height);
-  workspace.appendChild(preview);
+  preview.x = lastNodePosition + WorkspaceConstants.margin;
+  preview.y = WorkspaceConstants.height + WorkspaceConstants.margin;
 
   figma.currentPage.appendChild(workspace);
+  figma.currentPage.appendChild(preview);
 
   newNodes.push(workspace);
+  newNodes.push(preview);
+
   figma.currentPage.selection = newNodes;
   figma.viewport.scrollAndZoomIntoView(newNodes);
 };
 
 export const removeWorkspace = (msg: Msg) => {
+  const workspaceRegExp = new RegExp(
+    `^(?=.*${WorkspaceConstants.suffix})(?=.*${msg.workspaceName})`
+  );
+  const previewRegExp = new RegExp(
+    `^(?=.*${PreviewConstants.suffix})(?=.*${msg.workspaceName})`
+  );
   figma.currentPage
     .findAll(
       (node) =>
         node.type === 'FRAME' &&
-        node.name === WorkspaceConstants.namePrefix + msg.workspaceName
+        (workspaceRegExp.test(node.name) || previewRegExp.test(node.name))
     )
     .forEach((node) => {
       node.remove();
@@ -46,6 +57,7 @@ export const createPreview = (msg: Msg) => {
   figma.currentPage.findAll(
     (node) =>
       node.type === 'FRAME' &&
-      node.name === WorkspaceConstants.namePrefix + msg.workspaceName
+      node.name.includes(PreviewConstants.suffix) &&
+      node.name.includes(msg.workspaceName)
   );
 };
