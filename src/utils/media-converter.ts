61028d53751b532b8053e851828895ff1426d061
shuta13 <hollystarsun@gmail.com>
2021-06-02 11:48:48 +0900

    update: full size video without compressing

--- src/utils/media-converter.ts
+++ src/utils/media-converter.ts
@@ -7,18 +7,7 @@ const convertVideoToGif = async (file: File) => {
   }
   const fileName = file.name.split('.')[0];
   ffmpeg.FS('writeFile', `${file.name}`, await fetchFile(file));
-  // sourced: https://fireship.io/lessons/wasm-video-to-gif/
-  await ffmpeg.run(
-    '-i',
-    `${file.name}`,
-    '-t',
-    '2.5',
-    '-ss',
-    '2.0',
-    '-f',
-    'gif',
-    `${fileName}.gif`
-  );
+  await ffmpeg.run('-i', `${file.name}`, '-f', 'gif', `${fileName}.gif`);
   const data = ffmpeg.FS('readFile', `${fileName}.gif`);
   return {
     name: file.name,
